bdd69226471f437b67ed08bc8354245aad6abf6c
two-dimensional-array <max.mazyr2012@gmail.com>
2021-10-27 17:05:38 +0300

    Counting difference time with NTP server is implemented by cycle

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 #include <stdio.h>
 #include <time.h>
 
+#define NTP_REQUEST_PERIOD 30 //period for request difference with NTP server in seconds
+
 int main()
 {
-    printf("Difference with NTP server %f seconds\n", difftime(time(NULL), NTP_Update()));
+    time_t current_time = 0;
+    while (1)
+    {
+        if (difftime(time(NULL), current_time) > NTP_REQUEST_PERIOD)
+        {
+            time(&current_time);
+            printf("Difference with NTP server %f seconds\n", difftime(time(NULL), NTP_Update()));
+        }
+    }
     return 0;
 }
